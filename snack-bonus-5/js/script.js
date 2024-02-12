900e555936982e49dacfdfbdd357c62f017aeb26
Luca <lucafagiolini@live.it>
2024-02-12 18:41:09 +0100

    exercise completed

--- snack-bonus-5/js/script.js
+++ snack-bonus-5/js/script.js
@@ -1,5 +1,13 @@
 // chiedo una frase all'utente
-let parola = prompt('Inserisci una frase');
-console.log(parola);
+let frase = prompt('Inserisci una frase');
+console.log(frase);
 
 // cro uina funzione che applica la lettera maiuscola alla prima lettera della parola
+function capitalizeInitials(frase) {
+    return frase.replace(/\b\w/g, function (char) {
+        return char.toUpperCase();
+    });
+}
+
+console.log(capitalizeInitials(frase));
+document.getElementById('result').innerHTML = "result: (text-capitalize) " + capitalizeInitials(frase);
