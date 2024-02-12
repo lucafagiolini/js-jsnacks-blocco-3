85a036e139a2a04b25d6ba65baf6c226eb90080d
Luca <lucafagiolini@live.it>
2024-02-12 18:34:21 +0100

    sentence imput

---
+++ snack-bonus-5/js/script.js
@@ -0,0 +1,5 @@
+// chiedo una frase all'utente
+let parola = prompt('Inserisci una frase');
+console.log(parola);
+
+// cro uina funzione che applica la lettera maiuscola alla prima lettera della parola
