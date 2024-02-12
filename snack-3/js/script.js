f08d9249a49107bd9525353dbf4112644e44a16c
Luca <lucafagiolini@live.it>
2024-02-12 18:03:38 +0100

    funzione creata?

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -4,3 +4,12 @@ console.log(numbers);
 // creo un array di lettere
 let letters = ["a", "b", "c", "d", "e"];
 console.log(letters);
+
+
+function processArray(array, a, b) {
+    if (a < b && b <= array.length) {
+        return array.slice(a, b);
+    }
+}
+
+console.log(processArray(letters, 1, 3));
