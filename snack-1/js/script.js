776b6203d560eee5d20086df176b48a8a167b4fc
Luca <lucafagiolini@live.it>
2024-02-12 17:26:42 +0100

    snack-1 completed

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -5,5 +5,7 @@ let userWord = prompt("Inserisci una parola");
 function rigiraParole(parola) {
     return parola.split("").reverse().join("");
 }
+document.getElementById("result").innerText = "result: " + (rigiraParole(userWord));
 
 console.log(rigiraParole(userWord));
+
