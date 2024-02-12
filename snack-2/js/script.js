538383dca7b32dce93dc3dcd19b7333afe7d1891
Luca <lucafagiolini@live.it>
2024-02-12 17:47:21 +0100

    snack-2 completed

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,4 +1,19 @@
 // creo un array 
 let ham = ["pane", "ketchup", "cetrioli"];
+console.log(ham);
 // creo il secondo array
 let burger = ["hamburger", "formaggio", "insalata"];
+console.log(burger);
+
+// creo una funzione che mi permette di unire i due array
+function ominoMacdonalds(a, b) {
+    let result = [];
+    for (let i = 0; i < a.length; i++) {
+        result.push(a[i], b[i]);
+    }
+    return result;
+}
+
+// stampo il risultato
+console.log(ominoMacdonalds(ham, burger));
+document.getElementById("result").innerHTML = ominoMacdonalds(ham, burger);
